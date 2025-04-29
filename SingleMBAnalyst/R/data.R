e69709cddacd6a9dee459eafb827f1ff158b6258
Ping Wang <ping.wang@hrbmu.edu.cn>
2025-04-29 14:05:45 +0800

    Delete SingleMBAnalyst directory

--- SingleMBAnalyst/R/data.R
+++
@@ -1,7 +0,0 @@
-#' example data
-#'
-#' A Seurat object constructd by \code{\link{read_singleMB}}
-#'
-#' @format A Seurat object of five filtered 10X single cell samples.
-#'
-"seurat.object"
--- SingleMBAnalyst/README.md
+++
@@ -1,9 +0,0 @@
-# SingleMBAnalyst
-
-## Installation
-
-This can be installed using:
-
-```R
-devtools::install_github('PingWang7/SingleMBAnalyst')
-```
